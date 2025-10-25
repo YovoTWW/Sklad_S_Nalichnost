3ea9ff4736676c443072adf715cff52de2c1c03a
YovoTWW <yovo352@gmail.com>
2025-10-25 16:43:15 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.sklad_s_nalichnost {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.sklad_s_nalichnost to javafx.fxml;
+    exports com.example.sklad_s_nalichnost;
+}
